b40f49abe92de3a3fcc32132dc1269da09b81395
NanZ-hash <afnanxmt@gmail.com>
2020-03-29 21:26:45 +0300

    init the porject with a welcom message

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcom/index'
+
+  #adding the root : 
+  root 'welcom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
